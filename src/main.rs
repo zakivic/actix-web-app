d545207070bc045d1bb2b9fe9c4c7f7dee061591
zakaria <zakivic89@gmail.com>
2024-07-01 06:49:10 +0700

    added http app with 3 workers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::{thread, time};
-fn do_something(number: i8) -> i8 {
-    println!("number {} is running", number);
-    let two_seconds = time::Duration::new(2, 0);
-    thread::sleep(two_seconds);
-    return 2;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
 }
-fn main() {
-    let now = time::Instant::now();
-    let one: i8 = do_something(1);
-    let two: i8 = do_something(2);
-    let three: i8 = do_something(3);
-    println!("time elapsed {:?}", now.elapsed());
-    println!("result {}", one + two + three);
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        println!("http server factory is firing");
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
+    })
+    .bind("127.0.0.1:8080")?
+    .workers(3)
+    .run()
+    .await
 }
