00633203e95f0a5317956b9ee4518e9dab382de3
zakaria <zakivic89@gmail.com>
2024-07-22 15:55:01 +0700

    async with join multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use futures::executor::block_on;
+use futures::join;
 use std::{thread, time};
 
 async fn do_something(number: i8) -> i8 {
@@ -9,10 +10,14 @@ async fn do_something(number: i8) -> i8 {
 }
 fn main() {
     let now = time::Instant::now();
-    let future_one = do_something(1);
-    let two_seconds = time::Duration::new(2, 0);
-    thread::sleep(two_seconds);
-    let outcome = block_on(future_one);
+    let future_tree = async {
+        let outcome_one = do_something(1);
+        let outcome_two = do_something(2);
+        let results = join!(outcome_one, outcome_two);
+        return results.0 + results.1;
+    };
+
+    let outcome = block_on(future_tree);
     println!("time elapsed {:?}", now.elapsed());
     println!("Here is the outcome: {}", outcome);
 }
