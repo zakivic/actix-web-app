9a97a643181f3386887317d943d68f63d1c61e42
zakivic <zakivic89@gmail.com>
2024-07-27 16:09:50 +0700

    async actix experiment

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use async_std;
-use futures::executor::block_on;
-use futures::future::join_all;
-use std::vec::Vec;
-use std::{thread, time};
+use actix_web::{web, App, HttpServer, Responder};
+use futures::future;
 
-async fn do_something(number: i8) -> i8 {
-    println!("number {} is running", number);
-    let two_seconds = time::Duration::new(2, 0);
-    thread::sleep(two_seconds);
-    return 2;
+async fn utils_one() -> impl Responder {
+    "Utils one reached\n"
 }
-fn main() {
-    let async_outcome = async {
-        // 1.
-        let mut futures_vec = Vec::new();
-        let future_four = do_something(4);
-        let future_five = do_something(5);
-        // 2.
-        futures_vec.push(future_four);
-        futures_vec.push(future_five);
-        // 3.
-        let handles = futures_vec
-            .into_iter()
-            .map(async_std::task::spawn)
-            .collect::<Vec<_>>();
-        // 4.
-        let results = join_all(handles).await;
-        return results.into_iter().sum::<i8>();
-    };
-    let now = time::Instant::now();
-    let result = block_on(async_outcome);
-    println!("time elapsed for join vec {:?}", now.elapsed());
-    println!("Here is the result: {:?}", result);
+
+async fn health() -> impl Responder {
+    "All good\n"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let s1 = HttpServer::new(move || {
+            App::new().service(web::scope("/utils").route(
+            "/one", web::get().to(utils_one)))
+        })
+        .bind("0.0.0.0:3006")?
+        .run();
+
+    let s2 = HttpServer::new(move || {
+            App::new().service(web::resource("/health").route(web::get().to(health)))
+        })
+        .bind("0.0.0.0:8080")?
+        .run();
+
+    future::try_join(s1, s2).await?;
+    Ok(())
 }
